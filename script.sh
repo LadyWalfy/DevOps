1287908eab16a9135f18eb6ed32bd145b4948640
Alina <lady.walfy@yandex.ru>
2020-07-04 09:35:00 +0700

    First commit

---
+++ script.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+read a b c
+count=0
+
+
+
+if [ "$a" -le 0 ] || [ "$b" -le 0 ] || [ "$c" -le 0 ]; then
+       echo "Сторона треугольника должна быть больше 0"
+	exit 2 >&2      
+
+else
+       if [ "$(($a + $b))" -gt "$c" ] && [ "$(($a + $c))" -gt "$b" ] && [ "$(($b + $c))" -gt "$a" ]; then
+		if [ "$a" -eq "$b" ]; then 
+			count=$(($count + 1))
+		fi
+		if [ "$a" -eq "$c" ]; then 
+			count=$(($count + 1))
+        	fi
+		if [ "$b" -eq "$c" ]; then 
+			count=$(($count + 1))
+		fi
+		if [ "$count" -eq 0 ]; then
+			echo "Треугольник"
+			exit 0
+		fi
+		if [ "$count" -eq 1 ]; then
+			echo "Равнобедренный треугольник"
+			exit 0
+		fi
+		if [ "$count" -gt 1 ]; then
+			echo "Равносторонний треугольник"
+			echo true #exit 0
+		fi
+	else
+		echo "Не является треугольником"
+		exit 3 >&2
+	fi
+fi
+
